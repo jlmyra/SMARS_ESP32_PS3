67bff860e3322031489acc71ed04bc876cd5ff51
jlmyra <jlmyra@gmail.com>
2025-11-19 12:07:05 -0500

    Add files via upload

---
+++ Connection.ino
@@ -0,0 +1,18 @@
+
+
+//*******************Check controller Connection********************************
+ 
+void onConnection() {
+   
+  if (Ps3.isConnected()) {
+  Serial.println();
+  Serial.println();
+  Serial.println("********************************");
+  Serial.println("*** PS3 Controller Connected ***");
+  Serial.println("********************************");
+  Serial.println();
+  Serial.println();
+  }
+}
+
+//*******************END Check controller Connection*******************************
---
+++ Settings.h
@@ -0,0 +1,11 @@
+//Resistors
+
+//JLM's big motor bot
+//#define setR1 101300;
+//#define setR2 42300;
+//#define set_voltageCorrection 0.07;
+
+//Isabelle's Bot
+#define setR1 101900;
+#define setR2 41170;
+#define set_voltageCorrection 0.1;
